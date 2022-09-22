5deacc22631ad539ec708c4fab8ba9959c270444
Tomasz Bielecki <szkoleniegoit@gmail.com>
2022-09-22 22:25:50 +0200

    completed task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,23 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryDiv=document.querySelector(".gallery");
+
+galleryItems.forEach(element => {
+  const stringToHtml=`<a class="gallery__link" href="${element.original}"><img class="gallery__image" src="${element.preview}" data-source="${element.original}" alt="${element.description}"></img></a>`;
+  galleryDiv.insertAdjacentHTML("afterbegin",stringToHtml);
+});
+
+galleryDiv.addEventListener("click", (evt)=>{
+  evt.preventDefault();
+  if (evt.target.tagName!=="IMG") {
+    return
+  };
+  const instance = basicLightbox.create(`<img src="${evt.target.dataset.source}" width="800" height="600">`)
+  instance.show();
+  galleryDiv.addEventListener("keydown",()=>{
+    instance.close()
+  });
+});
+
